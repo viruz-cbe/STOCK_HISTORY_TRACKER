fb476d973bc04e2f46370e14fa61753db85abd5f
viruz-cbe <87322313+viruz-cbe@users.noreply.github.com>
2021-10-16 18:42:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#importing the required librabies
+import datetime as dt
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from mpl_finance import candlestick_ohlc
+
+start = dt.datetime(2021,1,1)
+end = dt.datetime.now()
+
+stock_name=input("ENTER THE STOCK NAME::")
+data = web.DataReader(stock_name,'yahoo',start,end)
+print(data.columns)
+data = data [['Open','High','Low','Close']]
+data.reset_index(inplace=True)
+data['Date']=data['Date'].map(mdates.date2num)
+
+ax = plt.subplot()
+ax.grid(True)
+ax.set_axisbelow(True)
+ax.set_facecolor('black')
+ax.figure.set_facecolor('#121212')
+ax.tick_params(axis='x',colors='white')
+ax.tick_params(axis='y',colors='white')
+ax.xaxis_date()
+candlestick_ohlc(ax, data.values, width=0.5,colorup='#00ff00')
+plt.show()
